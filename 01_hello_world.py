0ba98c8d9969aed94c54fb4b085b4150a1da4d11
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-02-28 11:59:44 -0300

    intensivo python

---
+++ 01_hello_world.py
@@ -0,0 +1,9 @@
+mesage = "Hello Python World!"
+print(mesage)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message_x = 'Eu disse ao meu amigo "Não quero isso"'
+
+print(message_x)
