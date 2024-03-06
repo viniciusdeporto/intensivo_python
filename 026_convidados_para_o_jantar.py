0c6276078d628f01ab67b26ed4281006f935e19a
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-03-06 14:41:53 -0300

    mais um commit finalizando o capitulo 3

---
+++ 026_convidados_para_o_jantar.py
@@ -0,0 +1,3 @@
+convidados = ['jesus', 'buda', 'enoque', 'moises', 'abrahao', 'fred mercury', 'elon musk']
+n_convidados = len(convidados)
+print(f"Os convidados serao {n_convidados}")
---
+++ 028_evitando_erros_de_indice_ao_trabalhar_com_listas.py
@@ -0,0 +1,6 @@
+motorcycle = ["honda", "yamaha", "suzuki"]
+# print(motorcycle[3])
+# sempre que quiser acessar o ultimo indeice da lista use o "-1"!
+print(motorcycle[-1]) 
+motos = []
+print(len(motos))
