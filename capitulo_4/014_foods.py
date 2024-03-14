4c655997f8c65bbb51b5e0369a7899d2198b144f
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-03-14 13:48:27 -0300

    commits

--- capitulo_4/014_foods.py
+++ capitulo_4/014_foods.py
@@ -1,6 +1,8 @@
 my_foods = ["pizza", "hamburguer", "frango frito", "batata frita"]
+#o sinal de [:] é fatia, é igual copiar, mas uma nao é igual a outra, é possivel adicionar elementos diferentes nos dois
 
-friend_foods = my_foods[:]
+# friend_foods = my_foods[:]
+friend_foods = my_foods
 
 my_foods.append("churrasco")
 
---
+++ capitulo_4/016_fatias.py
@@ -0,0 +1,5 @@
+coisas = ["livro", "comida", "canetas", "computador", "celular", "trabalho", "familia", "palheta"]
+
+print(f"Os tres primeiros elementos da lista são {coisas[:3]}")
+print(f"Os tres elementos no meio da lista são {coisas[2:5]}")
+print(f"Os tres ultimos elementos da lista são {coisas[-3:]}")
