c74e3539fd7b59ddb31193078803ab55cb1036df
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-03-15 00:09:36 -0300

    commit

---
+++ capitulo_4/020_buffet.py
@@ -0,0 +1,9 @@
+refeicoes = ("strogonoff", "carbonara", "empada", "pudim", "coxinha")
+for prato in refeicoes:
+    print(prato)
+print("------------------\n")
+
+refeicoes = ("strogonoff", "carbonara", "empada", "torta de maracuja", "sorvete")
+for prato in refeicoes:
+    print(prato)
+print("------------------\n")
