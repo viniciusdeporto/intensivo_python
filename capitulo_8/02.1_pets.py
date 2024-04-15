b4242943390a28bfea990e379a48b5bf1bffeb39
Vinicius De Porto <viniciusdeporto@hotmail.com>
2024-04-15 00:36:17 -0300

    commit do pc

--- capitulo_8/02.1_pets.py
+++ capitulo_8/02.1_pets.py
@@ -1,8 +1,8 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='dog'):
     '''Exibe as informações sobre um animal de estimação'''
     print(f'\nI have a {animal_type}.')
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet('dog', 'bob')
-describe_pet('hamster', 'stuart')
-describe_pet(pet_name='mel', animal_type='mel')
+describe_pet(pet_name='bob')
+
+describe_pet(pet_name='stuart', animal_type='hamster')
